5b25a3cf797e1c664afa6dee1fc903d8363cab5e
himanshugarg01 <47629005+himanshugarg01@users.noreply.github.com>
2019-03-09 21:00:52 +0530

    make edit and delete btn

--- script_by_me.js
+++ script_by_me.js
@@ -7,6 +7,7 @@ var products=[];
 aAddProduct.addEventListener("click",function(event)
 {
   createAddProductPannel();
+
 });
 function hideAddNewProductLink()
 {
@@ -88,9 +89,14 @@ function createAddProductPannel()
         else {
         alert("fill all textboxes ");
       }
+
+
     });
 
 }
+
+
+
 function addProducttoArray()
 {
 	var objProduct = new Object();
@@ -102,26 +108,30 @@ function addProducttoArray()
 	objProduct.Quantity = document.getElementById("prodQuantity").value;
 
     products.push(objProduct);
+    deleteList();
+    unHideAddNewProductLink();
 	makeTheList(objProduct);
   //  deleteNewProductPanel();
 	productId++;
 }
+
+
 function makeTheList(objProduct)
 {
   var listdiv=document.createElement("div");
-  listdiv.setAttribute("id",productId);
+  listdiv.setAttribute("id",objProduct.Id);
   divListProducts.appendChild(listdiv);
 
   var listprodname=document.createElement("a");
   listprodname.setAttribute("href","#");
+  listprodname.setAttribute("id","prodname"+objProduct.Id);
   listprodname.innerHTML=objProduct.Name;
   listdiv.appendChild(listprodname);
   //unHideAddNewProductLink();
   insertBlankLine(listdiv);
-  listprodname.addEventListener("click",function(event)
-{
   var listproddesc=document.createElement("p");
   //listproddesc.setAttribute("href","#");
+  listproddesc.setAttribute("id","proddesc"+objProduct.Id);
   listproddesc.innerHTML="Description of Product : "+objProduct.Desc;
   listdiv.appendChild(listproddesc);
 
@@ -129,6 +139,7 @@ function makeTheList(objProduct)
 
   var listprodprice=document.createElement("p");
   //listprodprice.setAttribute("href","#");
+listprodprice.setAttribute("id","prodprice"+objProduct.Id);
   listprodprice.innerHTML="Price : "+objProduct.Price;
   listdiv.appendChild(listprodprice);
 
@@ -136,6 +147,7 @@ function makeTheList(objProduct)
 
   var listprodqty=document.createElement("p");
   //listprodqty.setAttribute("href","#");
+  listprodqty.setAttribute("id","prodqty"+objProduct.Id);
   listprodqty.innerHTML="Quantity : "+objProduct.Quantity;
   listdiv.appendChild(listprodqty);
 
@@ -143,18 +155,148 @@ function makeTheList(objProduct)
 
   var delbtn=document.createElement("button");
   delbtn.innerHTML="delete";
-  delbtn.setAttribute("id","deletebtn");
+  delbtn.setAttribute("id","delbtn"+objProduct.Id);
   listdiv.appendChild(delbtn);
 
-  delbtn.addEventListener("click",function(event)
+  var editbtn=document.createElement("button");
+    editbtn.setAttribute("id","editbtn"+objProduct.Id);
+  editbtn.innerHTML="edit";
+  listdiv.appendChild(editbtn);
+
+  document.getElementById("proddesc"+objProduct.Id).setAttribute("style","visibility:hidden");
+  document.getElementById("prodprice"+objProduct.Id).setAttribute("style","visibility:hidden");
+  document.getElementById("prodqty"+objProduct.Id).setAttribute("style","visibility:hidden");
+  document.getElementById("delbtn"+objProduct.Id).setAttribute("style","visibility:hidden");
+  document.getElementById("editbtn"+objProduct.Id).setAttribute("style","visibility:hidden");
+
+  listprodname.addEventListener("mouseover",function(event)
+{
+  document.getElementById("proddesc"+objProduct.Id).setAttribute("style","visibility:visible");
+  document.getElementById("prodprice"+objProduct.Id).setAttribute("style","visibility:visible");
+  document.getElementById("prodqty"+objProduct.Id).setAttribute("style","visibility:visible");
+  document.getElementById("delbtn"+objProduct.Id).setAttribute("style","visibility:visible");
+  document.getElementById("editbtn"+objProduct.Id).setAttribute("style","visibility:visible");
+
+document.getElementById("delbtn"+objProduct.Id).addEventListener("click",function(event)
 {
   var target=event.target.parentNode;
+  deleteNodeFromArray(parseInt(target.id));
   target.parentNode.removeChild(target);
+});
+
+
+  document.getElementById("editbtn"+objProduct.Id).addEventListener("click",function(event)
+{
+ editPannel(objProduct);
 });
 });
 
+  listprodname.addEventListener("click",function()
+{
+    document.getElementById("proddesc"+objProduct.Id).setAttribute("style","visibility:hidden");
+    document.getElementById("prodprice"+objProduct.Id).setAttribute("style","visibility:hidden");
+    document.getElementById("prodqty"+objProduct.Id).setAttribute("style","visibility:hidden");
+    document.getElementById("delbtn"+objProduct.Id).setAttribute("style","visibility:hidden");
+    document.getElementById("editbtn"+objProduct.Id).setAttribute("style","visibility:hidden");
+});
 }
+
+  function editPannel(objProduct)
+  {
+    hideAddNewProductLink();
+    var label =document.createElement("label");
+    label.innerHTML = "Add New Product";
+    label.setAttribute("style","font-weight:bold");
+      divAddProduct.appendChild(label);
+
+      insertBlankLine(divAddProduct);
+      insertBlankLine(divAddProduct);
+
+      var prodName=document.createElement("input");
+      prodName.setAttribute("type","text");
+      prodName.setAttribute("id","prodName");
+      prodName.setAttribute("placeholder", "Enter the product name");
+      prodName.setAttribute("style","width:250px");
+      divAddProduct.appendChild(prodName);
+
+      insertBlankLine(divAddProduct);
+      insertBlankLine(divAddProduct);
+
+      var prodDesc=document.createElement("input");
+      prodDesc.setAttribute("id","prodDesc");
+        prodDesc.setAttribute("placeholder", "Enter the product description");
+      prodDesc.setAttribute("style","width:250px ; height:50px");
+      divAddProduct.appendChild(prodDesc);
+
+      insertBlankLine(divAddProduct);
+      insertBlankLine(divAddProduct);
+
+      var prodPrice = document.createElement("input");
+      prodPrice.setAttribute("type","text");
+      prodPrice.setAttribute("id","prodPrice");
+      prodPrice.setAttribute("placeholder", "Enter the product price");
+      prodPrice.setAttribute("style","width:250px");
+      divAddProduct.appendChild(prodPrice);
+
+      insertBlankLine(divAddProduct);
+      insertBlankLine(divAddProduct);
+
+      var prodQuantity = document.createElement("input");
+      prodQuantity.setAttribute("type","text");
+      prodQuantity.setAttribute("id","prodQuantity");
+        prodQuantity.setAttribute("placeholder", "Enter the product quantity");
+      prodQuantity.setAttribute("style","width:250px");
+      divAddProduct.appendChild(prodQuantity);
+
+      insertBlankLine(divAddProduct);
+      insertBlankLine(divAddProduct);
+
+      var btnAddButton = document.createElement("button");
+      btnAddButton.setAttribute("id","btnAddButton");
+      btnAddButton.innerHTML = "Add Product";
+      divAddProduct.appendChild(btnAddButton);
+
+
+      prodName.value=objProduct.Name;
+      prodDesc.value=objProduct.Desc;
+      prodPrice.value=objProduct.Price;
+      prodQuantity.value=objProduct.Quantity;
+
+      btnAddButton.addEventListener("click",function(event)
+      {
+
+        objProduct.Name = prodName.value;
+          objProduct.Desc = prodDesc.value;
+      	objProduct.Price = prodPrice.value;
+      	objProduct.Quantity = prodQuantity.value;
+        var a=document.getElementById("prodname"+objProduct.Id);
+        var b=document.getElementById("proddesc"+objProduct.Id);
+        var c=document.getElementById("prodprice"+objProduct.Id);
+        var d=document.getElementById("prodqty"+objProduct.Id);
+
+        a.innerHTML=prodName.value;
+        b.innerHTML="Description : "+prodDesc.value;
+        c.innerHTML="Price : "+prodPrice.value;
+        d.innerHTML="Quantity : "+prodQuantity.value;
+
+        deleteList();
+        unHideAddNewProductLink();
+      });
+
+
+  }
+
 function unHideAddNewProductLink()
 {
-  divAddProduct.setAttribute("style","visibility:visible");
+  aAddProduct.setAttribute("style","visibility:visible");
+}
+
+function deleteList()
+{
+  divAddProduct.innerHTML="";
+}
+
+function deleteNodeFromArray(a)
+{
+  products.splice(a,1);
 }
